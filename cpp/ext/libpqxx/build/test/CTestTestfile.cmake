f6e00522dbb54ed8b956530562307b6782c4b8f0
oeken <m.onureken@gmail.com>
2018-11-15 10:52:07 +0100

    remove makefiles

--- cpp/ext/libpqxx/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/oeken/Desktop/celonis-challenge/cpp/libpqxx/test
-# Build directory: /Users/oeken/Desktop/celonis-challenge/cpp/libpqxx/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "/Users/oeken/Desktop/celonis-challenge/cpp/libpqxx/build/test/runner")
-subdirs("unit")
--- cpp/ext/libpqxx/build/test/unit/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/oeken/Desktop/celonis-challenge/cpp/libpqxx/test/unit
-# Build directory: /Users/oeken/Desktop/celonis-challenge/cpp/libpqxx/build/test/unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_runner "/Users/oeken/Desktop/celonis-challenge/cpp/libpqxx/build/test/unit/unit_runner")
